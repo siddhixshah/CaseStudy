93ac8a756349ecd9b90bcaf6d1c2f6ab200f12e9
siddhixshah <newsletterforsid@gmail.com>
2025-12-15 21:46:52 +0530

    Fix HTML formatting in insight box content

--- electronics5.py
+++ electronics5.py
@@ -81,7 +81,7 @@ with c2:
     <b>Stop fighting the "Specs War". Start fighting the "Ecosystem War".</b><br>
     We cannot win on <i>Selling Phones</i> right now. We win by:<br>
     1. <b>Pivot to Contract Manufacturing:</b> Lease idle Noida/Pune lines to 'China+1' players.<br>
-    2. <b>Positioning Brand</b> where they aren't: <b>Sustainability (Green Tech).<br>
+    2. <b>Positioning Brand</b> where they aren't: <b>Sustainability (Green Tech).</b><br>
     3. <b>New PMF:</b> Capitalize on #RightToDisconnect Bill by competing on <i>Less Features</i>. Potential <b>₹250 Cr Blue Ocean</b> pan-India.<br>
     </div>
     """, unsafe_allow_html=True)
